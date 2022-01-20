ad6c777624320d6d9c2dc178177be5d4554c2abc
mai.mosaad.ahmed <mai.mosaad.ahmed@gmail.com>
2022-01-20 16:52:51 +0200

    changes2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PhoneService} from './core-module/services/phone.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webUI';
+  phonesData: any;
+  constructor(private phones:PhoneService) { 
+    
+    this.phones.getPhones().subscribe( (data) => {
+      this.phonesData = data;
+    });
+
+  }
 }
--- src/app/core-module/services/phone.service.ts
+++ src/app/core-module/services/phone.service.ts
@@ -1,17 +1,40 @@
 import { Injectable } from '@angular/core';
-// import {Http, Response} from "@angular/http";
-import 'rxjs/add/operator/map';
+import {HttpClient, HttpResponse} from "@angular/common/http";
+//import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhoneService {
 
-  // constructor(private http:HTTP) { }
-  constructor() { }
+  url = '/api/user';
+  phonesData:any ;
+  constructor(private http:HttpClient) { }
+ 
+  getPhones() {
+     return this.http.get(this.url);
+    //.map((res: Response) => res.json().HttpResponse);
+  //   return this.phonesData = [
+  //     {
+  //     "id":1,
+  //     "phone":"01010050529",
+  //     "country": "Egypt",
+  //     "countryCode":"+20",
+  //     "validityStatus":true
+  //   },
+  //   {
+  //     "id":2,
+  //     "phone":"01010050522",
+  //     "country": "Egypt",
+  //     "countryCode":"+20",
+  //     "validityStatus":true
+  //   },
+  // ]
+   
+  }
 
-  // getUser() {
-  //   return this.http.get('/api/user')
-  //     .map((res: Response) => res.json().response);
-  // }
+  postPhones(data:any) {
+    return this.http.post(this.url, data);
+ }
+
 }
